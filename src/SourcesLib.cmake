1a69c7546c14ab17dd1775ae0289fa959158e030
Benjamin Chrétien <chretien@lirmm.fr>
2014-03-29 21:38:15 +0100

    Add CMake support.

---
+++ src/SourcesLib.cmake
@@ -0,0 +1,12 @@
+SET(currentFolder "src/irrt")
+
+SET(JAVA_IRRT_SRC
+  #${currentFolder}/iRRT.form
+  ${currentFolder}/iRRT.java
+  #${currentFolder}/IRRTAboutBox.form
+  ${currentFolder}/IRRTAboutBox.java
+  ${currentFolder}/IRRTApp.java
+  ${currentFolder}/MiscMethods.java
+  ${currentFolder}/Node.java
+  ${currentFolder}/Tree.java
+)
